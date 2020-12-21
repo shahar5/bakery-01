9e0f0442f8390eeba47cef8aa8bd0c3c4b491a50
shahar5 <shahar.reuven@gmail.com>
2020-12-21 13:56:55 +0200

    syntax fixed

--- shahar's-PRs/challenge-#7/mount_ebs_vol.sh
+++ shahar's-PRs/challenge-#7/mount_ebs_vol.sh
@@ -4,17 +4,12 @@ DEVICE=/dev/xvdh
 FS_TYPE=$(file -s $DEVICE | awk '{print $2}')
 MOUNT_POINT=/home/ubuntu/data
 
-if [ "$DEVICE" != "/dev"]; then
-  # If no FS, then this output contains "data"
-  if [ "$FS_TYPE" = "data" ]; then
-      echo "Creating file system on $DEVICE"
-      mkfs -t xfs $DEVICE
-  fi
-
-  mkdir $MOUNT_POINT
-  mount $DEVICE $MOUNT_POINT
-  echo ${DEVICE} '/home/ubuntu/data xfs defaults 0 0' >> /etc/fstab
-
-else
-  echo "Something went wrong, check if volume exist on the VM"
+# If no FS, then this output contains "data"
+if [ "$FS_TYPE" = "data" ]; then
+    echo "Creating file system on $DEVICE"
+    mkfs -t xfs $DEVICE
 fi
+
+mkdir $MOUNT_POINT
+mount $DEVICE $MOUNT_POINT
+echo ${DEVICE} '/home/ubuntu/data xfs defaults 0 0' >> /etc/fstab
